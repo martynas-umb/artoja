b25dc90705d881c0802d527764e8b7546fa58eb4
Martynas <martynas@artme.lt>
2021-02-26 01:47:56 +0200

    add footer

--- wp-content/themes/artoja/resources/views/partials/footer.blade.php
+++ wp-content/themes/artoja/resources/views/partials/footer.blade.php
@@ -1,5 +1,86 @@
-<footer class="content-info">
+<div class="footer-area">
   <div class="container">
-{{--    @php(dynamic_sidebar('sidebar-footer'))--}}
+    <div class="row mb-40">
+      <div class="col-lg-12">
+        <div class="footer-content-wrapper border-top pt-40">
+          <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <!--=======  single footer widget  =======-->
+
+              <div class="single-footer-widget">
+                <div class="footer-logo mb-25">
+                  {!! wp_get_attachment_image(get_field('footer_logo','options'),'full') !!}
+                </div>
+
+                <div class="footer-text-block mb-10">
+                  <h5 class="footer-text-block__title">Susisiekime:</h5>
+                  <p class="footer-text-block__content"><a href="tel:+37069939452">+370 69 939 452</a></p>
+                </div>
+
+                <div class="footer-text-block mb-10">
+                  <h5 class="footer-text-block__title">Adresas:</h5>
+                  <p class="footer-text-block__content">Ukmergės g. 315A-6, Vilnius</p>
+                </div>
+                <div class="footer-text-block mb-10">
+                  <h5 class="footer-text-block__title">Darbo laikas:</h5>
+                  <p class="footer-text-block__content">I-IV 08:00 - 20:00<br>V 08:00 - 20:00</p>
+                </div>
+              </div>
+
+              <!--=======  End of single footer widget  =======-->
+            </div>
+
+            <div class="col-lg-6 col-md-6 mt-sm-30">
+              <!--=======  single footer widget  =======-->
+              <div class="footer-logo mb-25" style="opacity: 0">
+                {!! wp_get_attachment_image(get_field('footer_logo','options'),'full') !!}
+              </div>
+              <div class="single-footer-widget">
+                <h4 class="footer-widget-title">Informacija</h4>
+                <div class="footer-navigation">
+                  <nav>
+                    <ul>
+                      <li><a href="#">Apie mus</a></li>
+                      <li><a href="#">Kontaktai</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                      <li><a href="#">Privatumo politika</a></li>
+                      <li><a href="#">Taisyklės</a></li>
+                      <li><a href="#">Kaip pirkti ?</a></li>
+                      <li><a href="#">Apmokėjimas</a></li>
+                      <li><a href="#">Pristatymas</a></li>
+                      <li><a href="#">Garantija</a></li>
+                      <li><a href="#">Grąžinimas</a></li>
+                      <li><a href="#">Karjera</a></li>
+                      <li><a href="#">Privatumo pirkimo taisyklės</a></li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+
+              <!--=======  End of single footer widget  =======-->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="copyright-text-area">
+          <div class="row align-items-center">
+            <div class="col-md-6 text-center text-md-left mb-sm-15">
+              <div class="copyright-text">
+                <p>© 2021 <a href="#">Artoja</a></p>
+              </div>
+            </div>
+            <div class="col-md-6 text-center text-md-right">
+              <div class="payment-logo">
+                <img src="assets/img/icons/payment.png" class="img-fluid" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</footer>
+</div>
